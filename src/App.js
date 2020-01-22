c9abb599cc007de1a72ac2867e4f21c4bbe6fc23
Tedokon Ryota <s19014@std.it-college.ac.jp>
2020-01-22 17:02:19 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Autocomplete } from '@material-ui/lab'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+const App = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Autocomplete
+        id='combo-box-demo'
+        options={data}
+        //getOptionLabel={option => option.title}
+        style={{ width: 300 }}
+        renderInput={params => (
+          <TextField
+            {...params}
+            label='Combo box'
+            variant='outlined'
+            fullWidth
+          />
+        )}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
