7708a1bae2d935a21dd37b877ea3c5002315f011
Tedokon Ryota <s19014@std.it-college.ac.jp>
2020-01-23 14:35:09 +0900

    修正

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
 import React from 'react'
 import { Autocomplete } from '@material-ui/lab'
-import { TextField } from '@material-ui/core'
-import { Card } from '@material-ui/core'
+import { TextField, Card, CardActions, CardContent } from '@material-ui/core'
 import logo from './logo.svg'
 import './App.css'
 
-const App = props => {
-  const data = [
-    { name: 'tedokon', age: 23 },
-    { name: 'ryota', age: 20 },
-    { name: 'foo', age: 0 }
-  ]
-  return (
-    //<Card>
-    <div>
-      <Autocomplete
-        id='combo-box-demo'
-        options={data}
-        getOptionLabel={option => option.name}
-        style={{ width: 300 }}
-        renderInput={params => (
-          <TextField
-            {...params}
-            label='Combo box'
-            variant='outlined'
-            fullWidth
-          />
-        )}
-      />
-    </div>
-    //</Card>
-  )
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+      this.state = []
+  }
+
+  render () {}
 }
 
 export default App
