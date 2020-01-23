dc12c9d0ee61f688b0762a81f6d715328e4321e0
Tedokon Ryota <s19014@std.it-college.ac.jp>
2020-01-23 15:12:23 +0900

    test2

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import logo from './logo.svg'
 import './App.css'
 
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = []
+    }
+    return () {}
 
 }
 export default App
