03373dfc36c6602cdf66f0d01bcaac4a0079ab87
Tedokon Ryota <s19014@std.it-college.ac.jp>
2020-01-23 15:00:02 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import { Autocomplete } from '@material-ui/lab'
-import { TextField } from '@material-ui/core'
-import { Card } from '@material-ui/core'
+import { TextField, Card, CardActions, CardContent } from '@material-ui/core'
 import logo from './logo.svg'
 import './App.css'
 
-class App extends React.Component{}
+const ActionView = props => {
+}
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = []
+  }
+  handleChange (event) {
+    console.log(event.target)
+  }
+    render () {
+        return (
+            <Card>
+                <CardActions>
+                    <ActionView />
+                </CardActions>
+            </Card>
+        )
+    }
+}
 
 export default App
